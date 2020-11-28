bf725ee774eb124d5291ac2e507ef3523d0016f3
Hakan Sander <hakansander@gmail.com>
2020-11-28 15:48:12 +0300

    Add Spring JDBC way of DB operations (with both custom row mapper and default row mapper)

--- src/main/java/com/hakansander/embedded/h2embeddeddatabase/H2EmbeddedDatabaseApplication.java
+++ src/main/java/com/hakansander/embedded/h2embeddeddatabase/H2EmbeddedDatabaseApplication.java
@@ -1,13 +1,35 @@
 package com.hakansander.embedded.h2embeddeddatabase;
 
+import com.hakansander.embedded.h2embeddeddatabase.jdbc.PersonJdbcDao;
+import com.hakansander.embedded.h2embeddeddatabase.model.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+
 @SpringBootApplication
-public class H2EmbeddedDatabaseApplication {
+public class H2EmbeddedDatabaseApplication implements CommandLineRunner {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	PersonJdbcDao dao;
 
 	public static void main(String[] args) {
 		SpringApplication.run(H2EmbeddedDatabaseApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("All users -> {}", dao.findAll());
+		logger.info("User id 10001 -> {}", dao.findById(10001));
+		logger.info("Deleting user id 10002 -> {}", dao.deleteById(10002));
+		logger.info("Inserting 10004 -> {}", dao.insert(new Person(10004, "Tara", "Berlin", new Date())));
+		logger.info("All the people are listed -> {}", dao.findAll());
 	}
 
 }
